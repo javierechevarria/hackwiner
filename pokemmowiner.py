e9b1a6f20e49b1e4ec0a03e70770cd84b1363ad2
javierechevarria <echevarriajavier95@gmail.com>
2021-04-12 11:19:21 -0400

    Create pokemmowiner.py

---
+++ pokemmowiner.py
@@ -0,0 +1,7 @@
+#lineas de input
+id_pokemmo = str(input("ingrese su correo"))
+pass_pokemmo = str(input("ingrese contraseñ"))
+print(id_pokemmo)
+print(id_pokemmo)
+print("iniciando parche")
+#descarga de parche
